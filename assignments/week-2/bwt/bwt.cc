0e850d98fef3b2da0d977ae18245f3304eb6468a
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-02 11:55:53 +0000

    basic naive implementation of bwt.

--- assignments/week-2/bwt/bwt.cc
+++ assignments/week-2/bwt/bwt.cc
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <list>
 
 using std::cin;
 using std::cout;
@@ -9,16 +10,78 @@ using std::endl;
 using std::string;
 using std::vector;
 
-// write your code hereg
-string BWT(const string& text) {
-  string result = "";
+using namespace std;
 
-  return result;
+
+#ifdef DEBUG
+const bool debug = true;
+#else
+const bool debug = false;
+#endif
+
+
+void rotate(std::list<char> & pos){
+  pos.push_front(pos.back());
+  pos.pop_back();
 }
+
+/**
+ * Naive impementation of bwt
+ */
+string bwt(const string& text) {
+  int n  = text.size();
+
+  std::list<char> pos;
+  for(int i = 0; i < n; i++)
+    pos.push_back(text[i]);
+
+  vector<string> table;
+
+  for(int i = 0; i < n; i++) {
+    rotate(pos);
+    char s[n];
+    int j = 0;
+    for(char c : pos) {
+      s[j] = c;
+      j++;
+    }
+
+    string line(s,n);
+    table.push_back(line);
+  }
+  // sort them ?
+  sort(table.begin(),table.end());
+
+  if(debug) {
+    std::cerr<<"----------"<<endl;
+    for(int i = 0; i < n; i++) {
+      std::cerr<<table[i]<<endl;
+    }
+    std::cerr<<"----------"<<endl;
+  }
+
+  char r[n+1];
+  for(int i = 0; i < n ; i++) {
+    r[i] = table[i][n-1];
+  }
+  r[n] = '\0';
+
+  if(debug) {
+    for(int i = 0; i < n; i++) {
+      std::cerr<<r[i];
+    }
+    std::cerr<<endl;
+    std::cerr<<"*********"<<endl;
+  }
+
+  return string(r);
+}
+
+
 
 int main() {
   string text;
   cin >> text;
-  cout << BWT(text) << endl;
+  cout << bwt(text) << endl;
   return 0;
 }
