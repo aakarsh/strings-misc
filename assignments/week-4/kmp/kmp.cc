18a77d3c8c6dd1702ef47576b25fce5d8f0f21ad
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-03 13:17:06 +0000

    week-3 empty files

---
+++ assignments/week-4/kmp/kmp.cc
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::string;
+using std::vector;
+
+
+#ifdef DEBUG
+const bool debug = true;
+#else
+const bool debug = false;
+#endif
+
+/**
+ * Find all occurrences of the pattern in the text and return a
+ * vector with all positions in the text (starting from 0) where 
+ * the pattern starts in the text.
+ */
+vector<int> find_pattern(const string& pattern, const string& text) {
+  vector<int> result;
+
+  return result;
+}
+
+
+
+
+int main() {
+  string pattern, text;
+  cin >> pattern;
+  cin >> text;
+  vector<int> result = find_pattern(pattern, text);
+  for (int i = 0; i < result.size(); ++i) {
+    printf("%d ", result[i]);
+  }
+  printf("\n");
+  return 0;
+}
