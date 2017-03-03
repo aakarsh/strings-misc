2cc92998b5b59f8e8066b971355b4290b25f26d9
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-03 09:06:25 +0000

    Second pass at bwt inverse.

--- assignments/week-2/bwtinverse/bwtinverse.cc
+++ assignments/week-2/bwtinverse/bwtinverse.cc
@@ -10,6 +10,8 @@ using std::endl;
 using std::string;
 using std::vector;
 
+const char alphabet[] = {'$','A','C','G','T'};
+const int alphabet_size = 5;
 
 #ifdef DEBUG
 const bool debug = true;
@@ -17,81 +19,117 @@ const bool debug = true;
 const bool debug = false;
 #endif
 
+using namespace std;
+
+inline int char_index(char c) {
+  for(int i = 0; i< alphabet_size; i++)
+    if(alphabet[i] == c)
+      return i;
+}
+
+inline char index_char(int i) {
+  return alphabet[i];
+}
+
+void print_vector(string label, vector<int> v) {
+  if(!debug) 
+    return;
+  cerr<<label<<":";
+  for(int i : v)
+    cerr<<i<<" ";
+  cerr<<endl;
+}
+
+
 string inverse_bwt(const string& last) {
 
   int n  = last.size();
-  string text = "";
-  if(debug)
-    std::cerr<<last<<endl;
   string first(last);
-
   sort(first.begin(),first.end());
 
-  std::map<char,int> freq;
-
-  vector<int> rank(0,n);
+  vector<int> alphabet_frequency(alphabet_size,0);
+  vector<int> rank(n,0);
 
   int i = 0;
-
   for(char c : last)  {
-    int s = freq[c];
-    if(debug)
-      std::cerr<<c<<":"<<s<<",";
-    rank.push_back(s);
-    freq[c]=s+1;    
+    int cindex = char_index(c);
+    rank[i++] = alphabet_frequency[cindex];
+    alphabet_frequency[cindex] =  alphabet_frequency[cindex] + 1 ;
   }
-  if(debug)
-    std::cerr<<endl;
 
-  int total = 0;
+  print_vector("alphabet_frequency",alphabet_frequency);
+  print_vector("rank",rank);
+
+
+  vector<int> partial_sums(alphabet_size,0);
+  int partial_sum = 0;
+
+  for(int i = 0; i < alphabet_size;i++) {
+    partial_sums[i] = partial_sum;
+    partial_sum += alphabet_frequency[i];
+
+  }
+
+
+  print_vector("partial_sums",partial_sums);
+
+  if(debug) {
+    printf("%-5s %-5s %-5s\n","first","last","rank");
+    for(int i = 0; i < n; i++) {
+      printf("%-5c %-5c %-5d\n",first[i],last[i],rank[i]);
+    }
+    printf("\n");
+  }
+
+  
   char prev = '$';
-
   vector<char> result;
-  enum column {FIRST,LAST};
-
-  column current_column = LAST;
-  int c = 0;
   int lookup = 0;
+  int c = 0;
 
-  while(c < n) {
+  while (c < n) {
+    
     char cur = last[lookup];
-    int cur_rank = rank[lookup];
+    int  c_index = char_index(cur);
 
-    if(debug)
-      std::cerr<<"Looking for "<<cur<<" rank: "<<cur_rank<<std::endl;
+    if(debug) {
+      std::cerr<<"c_index:"<<c_index<<" cur ";
+      std::cerr<<cur<<" partial_sums :"<<partial_sums[c_index]<<endl;
+    }
 
-    int j = 0;
-    int h = 0;
+    lookup = partial_sums[c_index] + rank[lookup];
 
-    while(h < n) {
-      if(debug)
-        std::cerr<<"cur:"<<cur<<" first[h] "<<first[h]<<" cur_rank "<<cur_rank<<" j:"<<j<<std::endl;
-      if(cur_rank == j && cur == first[h]) {
-        lookup = h;
-        if(debug)
-          std::cerr<<"found@:"<<h<<endl;
-        break;
-      } else if(cur == first[h]) {
-        j++;
-      }
-      h++;
+    if(debug){
+      std::cerr<<"partial_sums :"<<partial_sums[c_index];
+      std::cerr<<",rank :"<<rank[c];
+      std::cerr<<",lookup :"<<lookup<<endl;
+    }
+
+    char first_char = first[lookup];
+    
+    if(debug) {
+      std::cerr<<"cur : "  << cur<<endl;
+      std::cerr<<"first: " << first_char<<endl;
+      std::cerr<<"pushing : " << cur<<endl;
     }
     if(cur!='$')
       result.push_back(cur);
-    c++;
-  }
 
+    c++;
+
+  }
+  
   std::reverse(result.begin(),result.end());
   result.push_back('$');
-
+    
   char res[n+1];
   i = 0;
   for(char c : result){
+
     res[i++] = c;
   }
   res[i] ='\0';
-
-  return string(res);
+  return string(res);   
 }
 
 int main() {
